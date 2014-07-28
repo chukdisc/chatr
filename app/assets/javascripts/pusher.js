26999661d2e42a002cbba67ae67cdfc80c889037
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-28 17:00:53 -0400

    WIP: Post chat messages with pusher

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -5,4 +5,14 @@ $(function(){
   channel.bind('chat-event', function(data) {
     console.log('An event was triggered with message: ' + data.message);
   });
+
+  $("#new_message").submit(function() {
+    $.post(
+      $(this).prop("action"),
+      $(this).serialize()
+    ).done(function(data){
+      $("#messages").prepend(data);
+    return false;
+  });
+
 });
