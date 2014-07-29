17dd8d35aa74e373de416c109ff0d676cf7dbef9
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-29 10:17:51 -0400

    Pusher appends new chat message to list

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -4,14 +4,17 @@ $(function(){
 
   channel.bind('chat-event', function(data) {
     console.log('An event was triggered with message: ' + data.message);
+    $("#messages").append(data.message);
   });
 
   $("#new_message").submit(function() {
     $.post(
       $(this).prop("action"),
       $(this).serialize()
-    ).done(function(data){
-      $("#messages").prepend(data);
+    ).done(function(renderedMessage){
+      $("#message_body").val("");
+    });
+
     return false;
   });
 
